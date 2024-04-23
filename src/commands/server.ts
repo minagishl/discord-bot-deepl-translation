1480e33aad7696de27540e7766f129b92ecd0313
minagishl <mvxd568.co@icloud.com>
2024-04-24 04:34:07 +0900

    revision

--- src/commands/server.ts
+++ src/commands/server.ts
@@ -8,43 +8,21 @@ import { COLOR } from '~/config';
 export default {
   data: new SlashCommandBuilder()
     .setName('server')
-    .setDescription('Returns the server information.')
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName('get-language')
-        .setDescription('Get the language of the server'),
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName('set-language')
-        .setDescription('Set the language of the server'),
-    ),
+    .setDescription('Get the current server language'),
 
   async execute(interaction: ChatInputCommandInteraction) {
     try {
-      const subcommand = interaction.options.getSubcommand();
-
-      if (subcommand === 'get-language') {
-        const languageCode = interaction.guild?.preferredLocale as string;
-        const languageName = new Intl.DisplayNames(['en'], {
-          type: 'language',
-        });
-        const embed = new EmbedBuilder()
-          .setColor(COLOR.PRIMARY)
-          .setTitle('Server Language')
-          .setDescription(
-            `The language of this server is ${languageName.of(languageCode)}.`,
-          );
-        await interaction.reply({ embeds: [embed] });
-      } else if (subcommand === 'set-language') {
-        const embed = new EmbedBuilder()
-          .setColor(COLOR.PRIMARY)
-          .setTitle('Server Language')
-          .setDescription(
-            'The language of this server has been set to English.',
-          );
-        await interaction.reply({ embeds: [embed] });
-      }
+      const languageCode = interaction.guild?.preferredLocale as string;
+      const languageName = new Intl.DisplayNames(['en'], {
+        type: 'language',
+      });
+      const embed = new EmbedBuilder()
+        .setColor(COLOR.PRIMARY)
+        .setTitle('Server Language')
+        .setDescription(
+          `The language of this server is ${languageName.of(languageCode)}.`,
+        );
+      await interaction.reply({ embeds: [embed] });
     } catch (err: any) {
       console.error(err);
     }
