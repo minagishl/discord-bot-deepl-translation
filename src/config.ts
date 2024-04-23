763beb64afc6e8c44d7084d932081857f2e59e4c
minagishl <mvxd568.co@icloud.com>
2024-04-24 02:14:01 +0900

    Add color

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,10 @@
-export const COLOR = {
+import { type ColorResolvable } from 'discord.js';
+
+type Color = Record<string, ColorResolvable>;
+
+export const COLOR: Color = {
   PRIMARY: '#ffffff',
   SECONDARY: '#000000',
+  DEEPL_BLUE: '#0F2B46',
   BLACK: '#1a1a1c',
 };
