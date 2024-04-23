cb910b2bef2741ffc8a8b6ce66b515ba750ac6e3
minagishl <mvxd568.co@icloud.com>
2024-04-24 04:45:11 +0900

    Add status command

---
+++ src/commands/status.ts
@@ -0,0 +1,24 @@
+import {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  type ChatInputCommandInteraction,
+} from 'discord.js';
+import { COLOR } from '~/config';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('status')
+    .setDescription('Check if the bot is working correctly.'),
+
+  async execute(interaction: ChatInputCommandInteraction) {
+    try {
+      const embed = new EmbedBuilder()
+        .setColor(COLOR.PRIMARY)
+        .setTitle('Status')
+        .setDescription(`If you got this reply, it's working correctly!`);
+      await interaction.reply({ embeds: [embed] });
+    } catch (err: any) {
+      console.error(err);
+    }
+  },
+};
